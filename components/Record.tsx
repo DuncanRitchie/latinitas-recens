1ad328aa1ff99aff453639a75e1f8da616556f0a
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2022-03-12 19:48:32 +0000

    Lemma is boldened in <Record>

--- components/Record.tsx
+++ components/Record.tsx
@@ -1,3 +1,5 @@
-export default function Record({record, q}) {
-    return (<p key={record}>{record}</p>)
+export default function Record(props: {record: string, q: string}) {
+    const {record, q} = props;
+    const [head, ...tail] = record.split('--');
+    return (<p key={record}><strong>{head}</strong>--{tail}</p>)
 }
