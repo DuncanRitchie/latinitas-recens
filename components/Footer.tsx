7a9c34dd33a308745337cfb35a95144559da01a5
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2022-03-12 22:23:09 +0000

    Footer calls LatinLexicon.org “Numen”

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
                 </a>
             </li>
             <li>
-                Data copied from <a href='http://web.archive.org/web/20080415072407/http://users.adelphia.net/~florusc/latine/search.html' title='Latinitas Recens, archived'>Florus’ Latinitas Recens</a> via <a href='https://latinlexicon.org/latinitas_recens_latine.php'>LatinLexicon.org</a>
+                Data copied from <a href='http://web.archive.org/web/20080415072407/http://users.adelphia.net/~florusc/latine/search.html' title='Latinitas Recens, archived'>Florus’ Latinitas Recens</a> via <a href='https://latinlexicon.org/latinitas_recens_latine.php' title='The Numen Latin lexicon'>Numen</a>
             </li>
             <li>
                 <a href='https://www.github.com/DuncanRitchie/latinitas-recens/'>
