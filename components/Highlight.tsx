a4b7a9d36db4640a309b42a0e3211b69bdf73e23
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2022-03-12 20:29:47 +0000

    Query is highlighted in records

---
+++ components/Highlight.tsx
@@ -0,0 +1,17 @@
+// Returns JSX for the `full` text with the `highlight` text in <mark> elements.
+// Eg, {full: "Fulmarus glacialis", highlight: "fulmar"} => <><mark>Fulmar</mark>us glacialis</>
+// Adapted from https://thewebdev.info/2021/11/13/how-to-highlight-text-using-react/
+export default function Highlight(props: {full: string, highlight: string}) {
+    const {full, highlight} = props;
+    const regex = new RegExp(`(${highlight.trim()})`, "i");
+    const parts = full.split(regex);
+    return (<>
+        {parts.filter(String).map((part, i) => {
+            return regex.test(part) ? (
+              <mark key={i}>{part}</mark>
+            ) : (
+              <span key={i}>{part}</span>
+            );
+          })}
+    </>)
+}
--- components/Record.tsx
+++ components/Record.tsx
@@ -1,5 +1,9 @@
+import Highlight from "./Highlight";
+
 export default function Record(props: {record: string, q: string}) {
     const {record, q} = props;
     const [head, ...tail] = record.split('--');
-    return (<p key={record}><strong>{head}</strong>--{tail}</p>)
+    const tailAsString = tail.join('--');
+
+    return (<p key={record}><strong><Highlight full={head} highlight={q}/></strong>--<Highlight full={tailAsString} highlight={q}/></p>)
 }
