68e438961d038145d74445ec1f7fa219b0701d6e
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2022-04-09 19:59:37 +0100

    Removed Next.js JS from production build

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,17 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  const isDev = process.env.NODE_ENV === "development";
   return (
     <Html>
       <Head>
         <link href="https://www.duncanritchie.co.uk/css/main.css" rel="stylesheet" key="main"/>
         <link href="https://www.duncanritchie.co.uk/velut-json-generator/main.css" rel="stylesheet" key="json-generator"/>
-        <base href="https://latinitas-recens.netlify.app/latinitas-recens/"/>
+        {!isDev && <base href="https://latinitas-recens.netlify.app/latinitas-recens/"/>}
       </Head>
       <body>
         <Main />
-        <NextScript />
+        {isDev && <NextScript />}
       </body>
     </Html>
   )
