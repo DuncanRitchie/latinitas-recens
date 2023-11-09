3b44433ab7e39b901a54d111d6d2a587d585c409
Duncan Ritchie <47029682+DuncanRitchie@users.noreply.github.com>
2023-11-09 22:06:57 +0000

    Used new font service

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ export default function Document() {
       <Head>
         <link href="https://www.duncanritchie.co.uk/css/main.css" rel="stylesheet" key="main"/>
         <link href="https://www.duncanritchie.co.uk/css/subsites.css" rel="stylesheet" key="subsites"/>
+        <link href="https://fonts.duncanritchie.co.uk/subsites.css" rel="stylesheet" key="fonts">
         {!isDev && <base href="https://latinitas-recens.netlify.app/latinitas-recens/"/>}
       </Head>
       <body>
